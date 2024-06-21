d5ba75913a48be0e9359e1b89f89864d512bce87
Armin Darvish <armindarvish@gmail.com>
2024-06-21 14:44:12 -0700

    update gptel cand title

--- sources/consult-omni-gptel.el
+++ sources/consult-omni-gptel.el
@@ -18,12 +18,49 @@
 (require 'consult-omni)
 
 ;;; Customization Variables
+
+(defcustom consult-omni-gptel-backend (or gptel-backend gptel--openai)
+  "LLM backend to use in consult-omni-gptel.
+
+By default inherits from `gptel-backend'.
+See `gptel-backend' for more info."
+  :type `(choice
+          (const :tag "ChatGPT" ,gptel--openai)
+          (restricted-sexp :match-alternatives (gptel-backend-p 'nil)
+           :tag "Other backend")))
+
+(defcustom consult-omni-gptel-model (or gptel-model "gpt-3.5-turbo")
+  "GPT Model for use in consult-omni-gptel.
+
+By default inherits from `gptel-model'.
+See `gptel-model' for more info."
+  :type '(choice
+          (string :tag "Specify model name")
+          (const :tag "GPT 3.5 turbo" "gpt-3.5-turbo")
+          (const :tag "GPT 3.5 turbo 16k" "gpt-3.5-turbo-16k")
+          (const :tag "GPT 4" "gpt-4")
+          (const :tag "GPT 4 turbo (preview)" "gpt-4-turbo-preview")
+          (const :tag "GPT 4 32k" "gpt-4-32k")
+          (const :tag "GPT 4 1106 (preview)" "gpt-4-1106-preview")))
+
+
 (defcustom consult-omni-gptel-buffer-name  "*consult-omni-gptel*"
   "Name for consult-omni-gptel buffer."
   :type '(choice (:tag "A string for buffer name" string)
                  (:tag "A custom function taking prompt (and other args) as input and returning buffer name string" function)))
 
-(cl-defun consult-omni--gptel-format-candidate (&rest args &key source query model backend stream face &allow-other-keys)
+(defcustom consult-omni-gptel-cand-title #'consult-omni--gptel-make-title-short-answer
+  "Name for consult-omni-gptel buffer."
+  :type '(choice (:tag "(Default) Get a quickshort answer" #'consult-omni--gptel-make-title-short-answer)
+                 (:tag "placeholder string with prompt" #'consult-omni--gptel-make-title-placeholder)
+                 (:tag "A custom function taking input (and other args) as input and returning a string" function)
+                 (:tag "A custom fixed string" string)))
+
+(defcustom consult-omni-gptel-short-answer-wordcount 10
+  "Number of words to use in a short answer"
+:type 'integer)
+
+(cl-defun consult-omni--gptel-format-candidate (title &rest args &key source query model backend stream face &allow-other-keys)
   "Returns a formatted string for gptel's candidates
 
 SOURCE is the name string of the source for candidate
@@ -41,13 +78,12 @@ SNIPPET is a string containing a snippet/description of candidate
 "
   (let* ((frame-width-percent (floor (* (frame-width) 0.1)))
          (source (and (stringp source) (propertize source 'face 'consult-omni-source-type-face)))
-         (title (format "ask gptel: %s" (if query (string-trim-right query) "")))
          (match-str (and (stringp query) (consult--split-escaped query) nil))
          (backend (and (stringp backend) (propertize backend 'face 'consult-omni-domain-face)))
          (model (and (stringp model) (propertize model 'face 'consult-omni-path-face)))
          (stream (and stream (propertize "~stream~" 'face 'consult-omni-snippet-face)))
          (face (or (consult-omni--get-source-prop source :face) face 'consult-omni-default-face))
-         (title-str (propertize title 'face face))
+         (title-str (and title (stringp title) (propertize title 'face face)))
          (title-str (consult-omni--set-string-width title-str (* 5 frame-width-percent)))
          (str (concat title-str
                       (when backend (concat "\t" backend))
@@ -81,11 +117,11 @@ If STREAM is non-nil, the response is streamed."
     (with-current-buffer (gptel (consult-omni--gptel-buffer-name query args) nil nil nil)
       (let* ((query-sent)
              (backend (and backend (car (seq-filter (lambda (item) (when (string-match (format "%s" backend) item) item)) (mapcar #'car gptel--known-backends)))))
-             (backend (or backend (gptel-backend-name gptel-backend)))
+             (backend (or backend (gptel-backend-name consult-omni-gptel-backend)))
              (backend (cdr (assoc (format "%s" backend) gptel--known-backends)))
              (model (or (and model (format "%s" model))
                         (and backend (car (cl-struct-slot-value (type-of backend) 'models backend)))
-                        gptel-model))
+                        consult-omni-gptel-model))
              (stream (if stream t nil))
              )
         (setq-local gptel-backend backend)
@@ -118,6 +154,64 @@ The preview buffer is from `consult-omni--gptel-response-preview'."
                  buff
                  ))))
 
+(cl-defun consult-omni--gptel-make-title-placeholder (input &rest args &key callback &allow-other-keys)
+(pcase-let* ((`(,query . ,opts) (consult-omni--split-command input (if callback (seq-difference args (list :callback callback)) args)))
+               (opts (car-safe opts))
+               (source "gptel")
+               (backend (and (plist-member opts :backend) (format "%s" (plist-get opts :backend))))
+               (backend (and backend (car (seq-filter (lambda (item) (when (string-match (format "%s" backend) item) item)) (mapcar #'car gptel--known-backends)))))
+               (backend (or backend (gptel-backend-name consult-omni-gptel-backend)))
+               (backend-struct  (cdr (assoc (format "%s" backend) gptel--known-backends)))
+               (model (and (plist-member opts :model) (format "%s" (plist-get opts :model))))
+               (model (or (and model backend-struct (member model (cl-struct-slot-value (type-of backend-struct) 'models backend-struct)) model)
+                          (and backend-struct (car (cl-struct-slot-value (type-of backend-struct) 'models backend-struct)))))
+               (stream (or (and (plist-member opts :stream) (plist-get opts :stream)) gptel-stream))
+               (placeholder (format "ask gptel: %s" (if query (string-trim-right query) "")))
+               (decorated (consult-omni--gptel-format-candidate placeholder :source source :query query :model model :backend backend :stream stream)))
+  (propertize decorated
+              :source source
+              :title query
+              :url nil
+              :query query
+              :model model
+              :stream stream
+              :backend backend)))
+
+(cl-defun consult-omni--gptel-make-title-short-answer (input &rest args &key callback &allow-other-keys)
+"Get a short answer preview from gptel."
+  (pcase-let* ((`(,query . ,opts) (consult-omni--split-command input (if callback (seq-difference args (list :callback callback)) args)))
+               (opts (car-safe opts))
+               (source "gptel")
+               (backend (and (plist-member opts :backend) (format "%s" (plist-get opts :backend))))
+               (backend (and backend (car (seq-filter (lambda (item) (when (string-match (format "%s" backend) item) item)) (mapcar #'car gptel--known-backends)))))
+               (backend (or backend (gptel-backend-name consult-omni-gptel-backend)))
+               (backend-struct (cdr (assoc (format "%s" backend) gptel--known-backends)))
+               (model (and (plist-member opts :model) (format "%s" (plist-get opts :model))))
+               (model (or (and model backend-struct (member model (cl-struct-slot-value (type-of backend-struct) 'models backend-struct)) model)
+                          (and backend-struct (car (cl-struct-slot-value (type-of backend-struct) 'models backend-struct)))
+                          consult-omni-gptel-model))
+               (stream (or (and (plist-member opts :stream) (plist-get opts :stream)) gptel-stream))
+               (gptel-backend backend-struct)
+               (gptel-model model)
+               (gptel-stream (if stream t nil))
+               (output nil))
+    (gptel-request query
+      :system (format "Respond in %s words or less." consult-omni-gptel-short-answer-wordcount)
+      :callback
+      (lambda (response _)
+        (when response
+          (let ((decorated
+                (consult-omni--gptel-format-candidate (string-trim-right response) :source source :query query :model model :backend backend :stream stream)))
+            (setq output (propertize decorated
+                        :title response
+                        :source "gptel"
+                        :url nil
+                        :query query
+                        :model model
+                        :stream stream
+                        :backend backend))))))
+output))
+
 (cl-defun consult-omni--gptel-fetch-results (input &rest args &key callback &allow-other-keys)
   "Makes cnaidate with INPUT as placeholder for `consult-omni-gptel'.
 
@@ -125,27 +219,12 @@ This makes a placeholder string “ask gptel: %s” %s=INPUT with
 metadata so it can be send to `gptel'."
   (unless (featurep 'gptel)
     (error "consult-omni: gptel is not available. Make sure to install and load `gptel'."))
-  (pcase-let* ((`(,query . ,opts) (consult-omni--split-command input (seq-difference args (list :callback callback))))
-               (opts (car-safe opts))
-               (source "gptel")
-               (backend (and (plist-member opts :backend) (format "%s" (plist-get opts :backend))))
-               (backend (and backend (car (seq-filter (lambda (item) (when (string-match (format "%s" backend) item) item)) (mapcar #'car gptel--known-backends)))))
-               (backend (or backend (gptel-backend-name gptel-backend)))
-               (backend-struct  (cdr (assoc (format "%s" backend) gptel--known-backends)))
-               (model (and (plist-member opts :model) (format "%s" (plist-get opts :model))))
-               (model (or (and model backend-struct (member model (cl-struct-slot-value (type-of backend-struct) 'models backend-struct)) model)
-                          (and backend-struct (car (cl-struct-slot-value (type-of backend-struct) 'models backend-struct)))))
-               (stream (or (and (plist-member opts :stream) (plist-get opts :stream)) gptel-stream))
-               (placeholder (consult-omni--gptel-format-candidate :source source :query query :model model :backend backend :stream stream))
-               (annotated-results (propertize placeholder
-                                              :source source
-                                              :title query
-                                              :url nil
-                                              :query query
-                                              :model model
-                                              :stream stream
-                                              :backend backend)))
-    (list annotated-results)
+  (when-let ((title (or (and (stringp consult-omni-gptel-cand-title) consult-omni-gptel-cand-title)
+                   (and (functionp consult-omni-gptel-cand-title)
+                        (funcall consult-omni-gptel-cand-title input args)))))
+    (when callback
+      (funcall callback (list title)))
+    (list title)
     ))
 
 (consult-omni-define-source "gptel"
