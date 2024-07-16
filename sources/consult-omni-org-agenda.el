cd897a44cfb5f938683f73b8e8339747d426fa9a
Armin Darvish <armindarvish@gmail.com>
2024-07-15 17:08:48 -0700

    improve consult-omni-org-agenda
    matches all words in the query

--- sources/consult-omni-org-agenda.el
+++ sources/consult-omni-org-agenda.el
@@ -34,13 +34,23 @@ See `consult-omni--org-agenda-transform-query' for details."
 
 
 (defcustom consult-omni-org-agenda-timestamp-format (or (and (bound-and-true-p org-timestamp-formats) (car org-timestamp-formats)) "%Y-%m-%d %a")
-"Timestamp fomrat for time string in `consult-omni-org-agenda'
+"Timestamp format for time string in `consult-omni-org-agenda'
 
 This string is used to fromat timestamps in the marginalia info.
 See `org-timestamp-formats' and `org-time-stamp-format'
 for org fomrating, and `format-time-string' for more details."
 
 :type 'string)
+
+
+(defcustom consult-omni-org-agenda-regexp-builder #'consult-omni--org-agenda-split-by-space
+"Function to transform query to a regexp pattern
+
+This funciton is called with the input query to get a
+matching regexp pattern"
+
+:type '(choice (const :tag "(Default) match any word sepearated by space" consult-omni--org-agenda-split-by-space)
+               (funciton :tag "custom function")))
 
 (defun consult-omni--org-agenda-date-range-regexp (date-strings)
 "Makes a regexp matching DATE-STRINGS trings.
@@ -117,10 +127,10 @@ otherwise get the date for DAYS days after DATE."
 Tries to guess the dates based on user input query.
 For example to get the date for tommorrow, next week, ..."
   (when (and consult-omni-org-agenda-transform-prefix (string-prefix-p consult-omni-org-agenda-transform-prefix query))
-    (setq query (string-remove-prefix consult-omni-org-agenda-transform-prefix query))
+    (setq query (string-remove-prefix consult-omni-org-agenda-transform-prefix query)))
     (cond
      ((equal query "yesterday") (consult-omni--org-agenda-previous-day (current-time)))
-     ((equal query "today") (format-time-string (car org-timestamp-formats) (current-time)))
+     ((equal query "today") (format-time-string consult-omni-org-agenda-timestamp-format (current-time)))
      ((equal query "tomorrow") (consult-omni--org-agenda-next-day (current-time)))
      ((equal query "this week") (consult-omni--org-agenda-date-range-regexp (consult-omni--org-agenda-week-of (current-time))))
      ((equal query "next week") (consult-omni--org-agenda-date-range-regexp (consult-omni--org-agenda-next-week (current-time))))
@@ -134,7 +144,7 @@ For example to get the date for tommorrow, next week, ..."
      ((string-match "around \\(.*\\)?" query)
       (when-let ((date (consult-omni--org-agenda-transform-query (concat consult-omni-org-agenda-transform-prefix (match-string 1 query)))))
         (consult-omni--org-agenda-date-range-regexp (consult-omni--org-agenda-around (or (car-safe date) date) consult-omni-org-agenda-number-of-days-around))))
-     (t nil))))
+     (t nil)))
 
 (cl-defun consult-omni--org-agenda-format-candidate (&rest args &key source query title buffer todo prio tags filepath snippet sched dead face &allow-other-keys)
   "Formats a candidate for `consult-omni-org-agenda' commands.
@@ -152,9 +162,9 @@ Description of Arguments:
          (match-str (if (and (stringp query) (not (equal query ".*"))) (consult--split-escaped query) nil))
          (buffer (and buffer (propertize (format "%s" buffer) 'face 'consult-omni-domain-face)))
          (prio (and (stringp prio) (propertize (format "[#%s]" prio) 'face 'consult-omni-prompt-face)))
-         (todo (and (stringp todo) (propertize todo 'face (or (and org-todo-keyword-faces (cdr (assoc todo org-todo-keyword-faces)))
+         (todo (or (and (stringp todo) (propertize todo 'face (or (and org-todo-keyword-faces (cdr (assoc todo org-todo-keyword-faces)))
                                                               (and (member todo org-done-keywords) 'org-done)
-                                                              'org-todo))))
+                                                              'org-todo))) ""))
          (tags (and tags (stringp tags) (propertize tags 'face 'consult-omni-keyword-face)))
          (snippet (and snippet (stringp snippet) (propertize snippet 'face 'consult-omni-snippet-face)))
          (snippet (if (stringp snippet) (consult-omni--set-string-width (replace-regexp-in-string "\n" "  " snippet) (* 2 frame-width-percent))))
@@ -167,10 +177,11 @@ Description of Arguments:
          (date (concat (and (stringp sched) sched) (and (stringp sched) " ") (and (stringp dead) dead)))
          (face (or (consult-omni--get-source-prop source :face) face))
          (todo-str (concat (or prio "    ") " " todo))
+         (todo-str (and (stringp todo-str) (consult-omni--set-string-width todo-str 15)))
          (title (if (and face (stringp title)) (propertize title 'face face) title))
          (title-str (if (and (stringp tags) (stringp title)) (concat title " " tags) title))
          (title-str (and (stringp title-str)
-                         (consult-omni--set-string-width title-str (* 5 frame-width-percent))))
+                         (consult-omni--set-string-width title-str (* 4 frame-width-percent))))
          (str (concat title-str
                       (and todo-str "\t") todo-str
                       (and buffer "\s") buffer
@@ -182,8 +193,13 @@ Description of Arguments:
          ((listp match-str)
           (mapcar (lambda (match) (setq str (consult-omni--highlight-match match str t))) match-str))
          ((stringp match-str)
-          (setq str (consult-omni--highlight-match match-str str t)))))
+          (setq str (consult-omni--highlight-match match-str str t)))
+        ))
     str))
+
+(defun consult-omni--org-agenda-split-by-space (query)
+  (string-join (split-string query "\s" t) "\\|")
+)
 
 (defun consult-omni--org-agenda-items (query &optional match &rest skip)
   "Return a list of Org heading candidates.
@@ -217,9 +233,9 @@ Adopted from `consult-org--headings'."
                     (sched (cdr (assoc "TIMESTAMP" props)))
                     (dead (cdr (assoc "DEADLINE" props)))
                     (snippet nil)
-                    (query (or (consult-omni--org-agenda-transform-query query) query)))
-         (if (string-match-p (concat ".*" query ".*") (concat todo " " prio " " _hl " " sched " " dead " " tags))
-             (propertize (consult-omni--org-agenda-format-candidate :source source :query query :title title :buffer buffer :todo todo :prio prio :tags tags :filepath filepath :snippet snippet :sched sched :dead dead) :source source :title title :query query :url nil :search-url nil :tags tags :filepath filepath :marker marker))))
+                    (transform (or (consult-omni--org-agenda-transform-query query) query)))
+         (if (string-match-p (or transform (funcall consult-omni-org-agenda-regexp-builder query)) (concat todo " " prio " " _hl " " sched " " dead " " tags))
+             (propertize (consult-omni--org-agenda-format-candidate :source source :query (or transform query) :title title :buffer buffer :todo todo :prio prio :tags tags :filepath filepath :snippet snippet :sched sched :dead dead) :source source :title title :query query :url nil :search-url nil :tags tags :filepath filepath :marker marker))))
      match 'agenda skip)))
 
 (defun consult-omni--org-agenda-preview (cand)
